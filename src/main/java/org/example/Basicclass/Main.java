8cdba0688d591336e053bb695707679359d2e230
sabari.r <sabari.r@ivymobility.com>
2025-09-24 13:50:39 +0530

    Create Base class

--- src/main/java/org/example/Basicclass/Main.java
+++ src/main/java/org/example/Basicclass/Main.java
@@ -1,9 +1,13 @@
-package org.example;
+package org.example.Basicclass;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Sampletest1git");
+     Student s1 = new Student();
+     s1.name = "Sabari";
+     s1.age = 20;
+     s1.display();
+     s1.sayHello("Kanth");
     }
 }
