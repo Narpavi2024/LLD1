80b534318821c6c9d3590a333927446b05c0ce2a
sabari.r <sabari.r@ivymobility.com>
2025-09-25 23:46:50 +0530

    Class Constructor

---
+++ src/main/java/OOPS2/Createclass/Classconstructor/PersonMain.java
@@ -0,0 +1,11 @@
+package OOPS2.Createclass.Classconstructor;
+
+public class PersonMain {
+    public static void main(String[] args) {
+        Person person = new Person(25,"Sabari");
+        Person person1 = new Person(24,"Kanth");
+        person.display();
+        person1.display();
+
+    }
+}
